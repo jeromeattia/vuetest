4b77d4e91abd76fe40cdd0c1be4faedd8989a308
jeromeattia <doc.jerome.attia@gmail.com>
2023-12-04 16:17:47 +0100

    port 3000

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  server: {
+    port: 3000,
+  },
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
